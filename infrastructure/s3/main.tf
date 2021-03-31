9e19d8b6dff153081185378e93c40ec294737b1b
Justin Paterson <jpaterson06@gmail.com>
2021-03-30 21:06:44 -0400

    Public access block

--- infrastructure/s3/main.tf
+++ infrastructure/s3/main.tf
@@ -23,3 +23,10 @@ resource "aws_s3_bucket" "deployer-s3" {
     enabled = true
   }
 }
+
+resource "aws_s3_account_public_access_block" "deployer-s3" {
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
